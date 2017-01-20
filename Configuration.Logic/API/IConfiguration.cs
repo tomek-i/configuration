dc1dcb30098f441bd4c22d8e50c571634b1977c3
Thomas Iwainski <tiwainski@warburnestate.com.au>
2017-01-20 11:30:21 +1100

    refactored

--- Configuration.Logic/API/IConfiguration.cs
+++ Configuration.Logic/API/IConfiguration.cs
@@ -3,7 +3,6 @@ using System.ComponentModel;
 
 namespace TI.Configuration.Logic.API
 {
-    //[TypeConverter(typeof(ConfigurationTypeConverter))]
     public interface IConfiguration: INotifyPropertyChanged
     {
         string Name { get; }
