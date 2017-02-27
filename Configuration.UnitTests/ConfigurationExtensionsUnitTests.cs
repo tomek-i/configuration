e13e15288c58213024a39347d9701f69670ffaa5
Thomas Iwainski <tiwainski@warburnestate.com.au>
2017-02-27 14:55:31 +1100

    refactored and added testcased

--- Configuration.UnitTests/ConfigurationExtensionsUnitTests.cs
+++ Configuration.UnitTests/ConfigurationExtensionsUnitTests.cs
@@ -1,30 +1,149 @@
-﻿using System.IO;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using NUnit.Framework;
-using TIConfiguration.Logic.API;
-using TIConfiguration.Logic._internals.Abstracts;
+using TI.Configuration.Logic;
+using TI.Configuration.Logic.Abstracts;
+using TI.Configuration.Logic.API;
 
 namespace TIConfiguration.UnitTests
 {
     [TestFixture]
     public class ConfigurationExtensionsUnitTests
     {
-        [TearDown]
-        public void Cleanup()
+        [TearDown, ExcludeFromCodeCoverage]
+        public void CleanUp()
         {
-            // Thread.Sleep(300);
-            if (Directory.Exists("./configs"))
-                Directory.Delete("./configs", true);
+            if(Directory.Exists(ConfigurationManager.DefaultPath))
+                Directory.Delete(ConfigurationManager.DefaultPath, true);
+        }
+        [ExcludeFromCodeCoverage]
+        public class NotInternalCfg : ConfigurationBase
+        {
+            public string Value { get; set; } = "old";
         }
 
         [InternalConfiguration]
-        internal class InternalConfig : ConfigurationBase
+        public class InternalCfg : ConfigurationBase
         {
-            public int TestValue { get; set; } = 123;
+
         }
 
-        internal class RegularConfig : ConfigurationBase
+        [InternalConfiguration("abc","123")]
+        public class InternalCustomCfg : ConfigurationBase
         {
-            public string Value { get; set; } = "old";
+
+        }
+
+        public IConfiguration GetDefaultInstanceOf<T>() where T:ConfigurationBase, new()
+        {
+            return new T();
+        }
+
+        [Test] public void GetFilePath_NotInternalCfg_ReturnCorrectFolderandFilename()
+        {
+            //ARRANGE
+            IConfiguration cfg = GetDefaultInstanceOf<NotInternalCfg>();
+            string expected = $"\\debug\\{nameof(NotInternalCfg)}.json";
+            //ACT
+            string result = cfg.GetFilePath();
+            //ASSERT
+            StringAssert.Contains(expected, result);
+        }
+        [Test] public void GetFilePath_InternalCfg_ReturnCorrectFolderandFilename()
+        {
+            //ARRANGE
+            IConfiguration cfg = GetDefaultInstanceOf<InternalCfg>();
+            string expected = $"_internals\\_{nameof(InternalCfg)}.json";
+            //ACT
+            string result = cfg.GetFilePath();
+            //ASSERT
+            StringAssert.Contains(expected, result);
+        }
+        [Test] public void GetFilePath_InternalCustomCfg_ReturnCorrectFolderandFilename()
+        {
+            //ARRANGE
+            IConfiguration cfg = GetDefaultInstanceOf<InternalCustomCfg>();
+            string expected = $"abc\\123{nameof(InternalCustomCfg)}.json";
+            //ACT
+            string result = cfg.GetFilePath();
+            //ASSERT
+            StringAssert.Contains(expected,result);
+        }
+        [Test] public void GetInternalConfig_NotDecorated_Null()
+        {
+            //ARRANGE
+            IConfiguration cfg = GetDefaultInstanceOf<NotInternalCfg>();
+            //ACT
+            InternalConfigurationAttribute result = cfg.GetInternalConfig();
+            //ASSERT
+            Assert.IsNull(result);
+        }
+        [Test] public void GetInternalConfigProperty_FilePrefix_HasDefaultValues()
+        {
+            //ARRANGE
+            IConfiguration cfg = GetDefaultInstanceOf<InternalCfg>();
+            //ACT
+            string value = cfg.GetInternalConfigProperty(x => x.FilePrefix);
+            //ASSERT
+            Assert.IsNotNullOrEmpty(value);
+        }
+        [Test] public void GetInternalConfigProperty_FolderName_HasDefaultValues()
+        {
+            //ARRANGE
+            IConfiguration cfg = GetDefaultInstanceOf<InternalCfg>();
+            //ACT
+            string value = cfg.GetInternalConfigProperty(x => x.Foldername);
+            //ASSERT
+            Assert.IsNotNullOrEmpty(value);
+        }
+
+        [Test] public void GetInternalConfigProperty_CustomCfgFolderName_HasCustomValue()
+        {
+            //ARRANGE
+            IConfiguration cfg = GetDefaultInstanceOf<InternalCustomCfg>();
+            //ACT
+            string value = cfg.GetInternalConfigProperty(x => x.Foldername);
+            //ASSERT
+            Assert.IsNotNullOrEmpty(value);
+            Assert.AreEqual("abc",value);
+        }
+        [Test] public void GetInternalConfigProperty_CustomCfgFilePrefix_HasCustomValue()
+        {
+            //ARRANGE
+            IConfiguration cfg = GetDefaultInstanceOf<InternalCustomCfg>();
+            //ACT
+            string value = cfg.GetInternalConfigProperty(x => x.FilePrefix);
+            //ASSERT
+            Assert.IsNotNullOrEmpty(value);
+            Assert.AreEqual("123", value);
+        }
+        [Test] public void GetInternalConfig_Decorated_NotNull()
+        {
+            //ARRANGE
+            IConfiguration cfg = GetDefaultInstanceOf<InternalCfg>();
+            //ACT
+            InternalConfigurationAttribute result = cfg.GetInternalConfig();
+            //ASSERT
+            Assert.IsNotNull(result);
+        }
+        [Test] public void IsInternalConfiguration_DecoratedClass_True()
+        {
+            //ARRANGE
+            IConfiguration cfg = GetDefaultInstanceOf<InternalCfg>();
+            //ACT
+            bool result = cfg.IsInternalConfiguration();
+            //ASSERT
+            Assert.IsTrue(result);
+        }
+        [Test] public void IsInternalConfiguration_NotDecoratedClass_False()
+        {
+            //ARRANGE
+            IConfiguration cfg = GetDefaultInstanceOf<NotInternalCfg>();
+            //ACT
+            bool result = cfg.IsInternalConfiguration();
+            //ASSERT
+            Assert.IsFalse(result);
         }
 
 
@@ -32,7 +151,7 @@ namespace TIConfiguration.UnitTests
         public void IsInternalConfiguration_OnConfiguration_ReturnFalse()
         {
             //ARRANGE
-            RegularConfig cfg = new RegularConfig();
+            NotInternalCfg cfg = new NotInternalCfg();
             //ACT
             var result = cfg.IsInternalConfiguration();
             //ASSERT
@@ -43,7 +162,7 @@ namespace TIConfiguration.UnitTests
         public void IsInternalConfiguration_OnInternalConfiguration_ReturnTrue()
         {
             //ARRANGE
-            InternalConfig cfg = new InternalConfig();
+            InternalCfg cfg = new InternalCfg();
             //ACT
             var result = cfg.IsInternalConfiguration();
             //ASSERT
@@ -51,9 +170,22 @@ namespace TIConfiguration.UnitTests
         }
 
         [Test]
+        public void GetInternalConfigProperty_OnNonInternalConfig_Throws()
+        {
+            //ARRANGE
+            NotInternalCfg cfg = new NotInternalCfg();
+            //ACT
+            Assert.Throws<InvalidOperationException>(delegate
+            {
+                var result = cfg.GetInternalConfigProperty(x=>x.Foldername);
+            });
+            
+            //ASSERT
+        }
+        [Test]
         public void GetInternalConfig_OnInternalConfiguration_ReturnInstance()
         {
-            InternalConfig cfg = new InternalConfig();
+            InternalCfg cfg = new InternalCfg();
             var result = cfg.GetInternalConfig();
 
             Assert.IsNotNull(result);
@@ -62,7 +194,7 @@ namespace TIConfiguration.UnitTests
         [Test]
         public void GetInternalConfig_OnInternalConfiguration_ReturnAttribute()
         {
-            InternalConfig cfg = new InternalConfig();
+            InternalCfg cfg = new InternalCfg();
             var result = cfg.GetInternalConfig();
 
             Assert.IsInstanceOf<InternalConfigurationAttribute>(result);
@@ -71,86 +203,10 @@ namespace TIConfiguration.UnitTests
         [Test]
         public void GetInternalConfig_OnRegularConfiguration_ReturnNull()
         {
-            RegularConfig cfg = new RegularConfig();
+            NotInternalCfg cfg = new NotInternalCfg();
             var result = cfg.GetInternalConfig();
 
             Assert.IsNull(result);
-        }
-
-
-        [Test, Category("Integration Tests")]
-        public void Write_Configuration_FileExists()
-        {
-            RegularConfig cfg = new RegularConfig();
-            cfg.Write();
-            Assert.IsTrue(File.Exists("./configs/debug/RegularConfig.json"));
-        }
-        [Test, Category("Integration Tests")]
-        public void Write_Configuration_ReturnsTrue()
-        {
-            RegularConfig cfg = new RegularConfig();
-            var result = cfg.Write();
-
-            Assert.IsTrue(result);
-        }
-
-        [Test, Category("Integration Tests")]
-        public void Reload_ExistingCfgFile_ReturnsNewInstance()
-        {
-           RegularConfig actual = new RegularConfig();
-            actual.Value = "testvalue";
-            actual.Write();
-
-            var cfgUnderTest = actual.Reload<RegularConfig>();
-
-            Assert.AreNotSame(actual,cfgUnderTest);
-        }
-        [Test,Category("Integration Tests")]
-        public void Reload_ExistingCfgFile_InstanceReturnedHasCorrectValue()
-        {
-            new RegularConfig {Value = "testvalue"}.Write();
-
-            var cfgUnderTest = new RegularConfig().Reload<RegularConfig>();
-
-            Assert.AreEqual("testvalue",cfgUnderTest.Value);
-        }
-        [Test, Category("Integration Tests")]
-        public void Reload_NonExistingCfgFile_ReturnsSameInstance()
-        {
-            var cfg = new RegularConfig { Value = "testvalue" };
-            
-            var cfgUnderTest = cfg.Reload<RegularConfig>();
-
-            Assert.AreSame(cfg,cfgUnderTest);
-        }
-
-        [Test, Category("Integration Tests")]
-        public void Update_NonExistingCfgFile_WritesCfg()
-        {
-            var cfg = new RegularConfig();
-
-            cfg.Update();
-
-            Assert.IsTrue(File.Exists("./configs/debug/RegularConfig.json"));
-        }
-        [Test, Category("Integration Tests")]
-        public void Update_ExistingCfgFile_OverWritesCfgWithNewValue()
-        {
-            var cfg = new RegularConfig();
-
-            cfg.Update<RegularConfig>(x=>x.Value = "NewValue");
-
-            var cfgUnderTest = new RegularConfig().Reload<RegularConfig>();
-            Assert.AreEqual("NewValue", cfgUnderTest.Value);
-        }
-        [Test, Category("Integration Tests")]
-        public void Update_ExistingCfgFile_SetsPropertyToNewValue()
-        {
-            var cfg = new RegularConfig();
-
-            cfg.Update<RegularConfig>(x => x.Value = "NewValue");
-
-            Assert.AreEqual("NewValue", cfg.Value);
         }
 
 
