22b6bdb5e3a1211c2e3df20c1752596752d75da9
Thomas Iwainski <tiwainski@warburnestate.com.au>
2017-02-27 14:54:41 +1100

    refactored and added codecoverage exclusions

--- Configuration.Logic/Abstracts/ConfigurationBase.cs
+++ Configuration.Logic/Abstracts/ConfigurationBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using TI.Configuration.Logic.API;
 using TI.Configuration.Logic.Properties;
@@ -9,12 +10,17 @@ namespace TI.Configuration.Logic.Abstracts
     [Serializable]
     public abstract class ConfigurationBase : IConfiguration
     {
+        [ExcludeFromCodeCoverage]
         public event PropertyChangedEventHandler PropertyChanged;
         public string Name => GetType().Name;
+
+        [ExcludeFromCodeCoverage]
         public DateTime Created { get; }
+        [ExcludeFromCodeCoverage]
         public DateTime Changed { get; private set; }
 
         [NotifyPropertyChangedInvocator]
+        [ExcludeFromCodeCoverage]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
@@ -25,14 +31,14 @@ namespace TI.Configuration.Logic.Abstracts
             Created = DateTime.Now;
             PropertyChanged += OnPropertyChanged;
         }
-
+        [ExcludeFromCodeCoverage]
         protected virtual void OnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
         {
             Changed = DateTime.Now;
         }
 
         #region Overrides of Object
-
+        [ExcludeFromCodeCoverage]
         public override string ToString()
         {
             return Name;
