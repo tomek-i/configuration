d1134ccb5eb344772f5dfb2ae9525c99be96bc40
Thomas Iwainski <tiwainski@warburnestate.com.au>
2017-11-07 09:24:29 +1100

    Refactor

--- Configuration.Logic/API/IConfiguration.cs
+++ Configuration.Logic/API/IConfiguration.cs
@@ -6,6 +6,11 @@ namespace TI.Configuration.Logic.API
     {
         string Name { get; }
         DateTime Created { get; }
+
+        /// <summary>
+        /// Creates a new instance with default values.
+        /// </summary>
+        /// <returns>A new instance with valid default values.</returns>
         IConfiguration Default();
     }
 }
