8c571146112f54f06030fe1ece5d20bd22854379
Thomas Iwainski <tiwainski@warburnestate.com.au>
2018-10-16 14:15:56 +1100

    refactor

--- Configuration.Logic/Extensions/ConfigurationExtensions.cs
+++ Configuration.Logic/Extensions/ConfigurationExtensions.cs
@@ -3,11 +3,22 @@ using System.IO;
 using System.Reflection;
 using TI.Configuration.Logic.API;
 using TI.Configuration.Logic._internals.Configs;
+using System.Linq.Expressions;
 
 namespace TI.Configuration.Logic
 {
     public static class ConfigurationExtensions
     {
+        public static string GetTooltip<T, TProp>(this T o, Expression<Func<T, TProp>> propertySelector) where T : Configuration.Logic.Abstracts.ConfigurationBase
+        {
+            string propertyName = ((MemberExpression)propertySelector.Body).Member.Name;
+            return ((MemberExpression)propertySelector.Body).Expression.Type.GetProperty(propertyName).GetCustomAttribute<ConfigurationToolTipAttribute>()?.TooltipText;
+        }
+
+        public static string GetTooltip<T>(this T o, string propertyName) where T : Configuration.Logic.Abstracts.ConfigurationBase
+        {
+            return o.GetType().GetProperty(propertyName).GetCustomAttribute<ConfigurationToolTipAttribute>()?.TooltipText;
+        }
 
         public static bool IsInternalConfiguration(this IConfiguration config)
         {
